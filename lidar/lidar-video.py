a17c234b8f3032dbbc6ce3a22862d50722d9dec8
James Jelenko <jamesjelenko@gmail.com>
2024-01-11 01:02:39 -0800

    [1-10-24] add distance check with warning when proximity < 1, update GUI To reflect warning

--- lidar/lidar-video.py
+++ lidar/lidar-video.py
@@ -5,6 +5,9 @@ import tkinter as tk
 from lidar import get_lidar_distance
 from PIL import Image, ImageTk
 
+def draw_target_mark(img):
+    cv2.circle(img, (100,100),63,(0,0,255), 1)
+
 # Function to update GUI with new frame
 def update_gui():
     try:
@@ -13,17 +16,24 @@ def update_gui():
 
         # Get distance from TF-Luna Lidar (replace this with your actual implementation)
         lidar_distance = get_lidar_distance()
-
+        warning_text = f"WARNING! {lidar_distance} m! Too close!"
+        overlay_text = None
+        color_tuple = (0,255,0)
         # Overlay lidar distance on the frame
         if lidar_distance is not None:
-            overlay_text = f"Lidar Distance: {lidar_distance} cm"
-            cv2.putText(frame, overlay_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            overlay_text = f"Lidar Distance: {lidar_distance} m"
+            if lidar_distance <= 1:
+                overlay_text = warning_text
+                color_tuple = (0,0,255)
+            cv2.putText(frame, overlay_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color_tuple, 2)
 
         # Convert frame to RGB format for tkinter
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Convert to ImageTk format
+        draw_target_mark(frame_rgb)
         img = Image.fromarray(frame_rgb)
+        #draw_target_mark(img)
         img_tk = ImageTk.PhotoImage(image=img)
 
         # Update the label in the GUI
