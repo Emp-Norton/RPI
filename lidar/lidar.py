c9475384f6e887d760d8c27b21d9a57a8201f654
James Jelenko <jamesjelenko@gmail.com>
2024-01-10 23:32:40 -0800

    [1-10-24] add check for serial port open, add coutner for bytes in waiting, now functional

--- lidar/lidar.py
+++ lidar/lidar.py
@@ -7,17 +7,22 @@ ser = serial.Serial('/dev/serial0', baudrate=115200, timeout=1)
 # Function to get Lidar distance
 
 def get_lidar_distance():
+    if not ser.isOpen():
+      ser.open()
     try:
         # Send command to TF-Luna
         ser.write(b'\x42\x57\x02\x00\x00\x00\x01\x06')
 
         # Read and parse response
-        response = ser.read(9)
-        if len(response) == 9 and response[0] == 0x59 and response[1] == 0x59:
-            distance = (response[2] + response[3] * 256) / 100.0
-            return distance
-        else:
-            print("Invalid response from TF-Luna")
+        count = ser.in_waiting
+        if count > 8:
+            response = ser.read(9)
+            ser.reset_input_buffer()
+            if len(response) == 9 and response[0] == 0x59 and response[1] == 0x59:
+                distance = (response[2] + response[3] * 256) / 100.0
+                return distance
+            else:
+                print("Invalid response from TF-Luna")
     except Exception as e:
         print(f"Error: {e}")
 
