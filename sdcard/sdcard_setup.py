a0c74ba9df41f7fb9959152c2b4a20f74dd0cdea
James Jelenko <jamesjelenko@gmail.com>
2025-01-29 20:31:54 -0800

    add internal initialization functions for sdcard module class

--- sdcard/sdcard_setup.py
+++ sdcard/sdcard_setup.py
@@ -22,9 +22,44 @@ class SDCardModule:
 	mosi (microcontroller.Pin): The MOSI (Master Out Slave In) pin, e.g., board.GP8
 	sck  (microcontroller.Pin): The SCLK (Serial Clock) pin, e.g., board.GP11
 	cs   (microcontroller.Pin): The CS (Chip Select) pin, e.g., board.GP10
+	mount_path (str): String representing where the virtual file system will mount the sdcard module FS.
 
     Example:
     >>> import board
     >>> sd_card = SDCardModule(sck=board.GP11, miso=board.GP9,
 				mosi=board.GP8, cs=board.GP10)
     """
+    def __init__(self, miso, mosi, cs, sck, mount_path="/sd"):
+        self.miso = miso
+	self.mosi = mosi
+	self.cs = cs
+	self.sck = sck
+	self.mount_path = mount_path
+
+    def __init_spi(self):
+	try:
+	    self.spi = busio.SPI(self.sck, MOSI=self.mosi, MISO=self.miso)
+
+	except Exception as e:
+	    print(f"Failed to initialize SPI object: {e}")
+
+    def __init_sdcard(self):
+	try:
+	    self.sdcard = sdcardio.SDCard(self.spi, self.cs)
+
+	except Exception as e:
+	    print(f"Failed to create SDCard object: {e}")
+
+    def __init_virtual_fs(self):
+	try:
+	    self.vfs = storage.VfsFat(sdcard)
+
+	except Exception as e:
+	    print(f"Failed to virtualize filesystem: {e}")
+
+    def __mount_vfs(self):
+	try:
+	    storage.mount(self.vfs, self.mount_path)
+
+	except Exception as e:
+	    print(f"Unable to mount {self.mount_path}: {e}")
