afe1e59c18dac0da304b2aaa93a6e6afa355cff2
James Jelenko <jamesjelenko@gmail.com>
2025-01-29 18:52:01 -0800

    add basic sdcard module setup logic

---
+++ sdcard/sdcard_setup.py
@@ -0,0 +1,12 @@
+import board, busio, os, sdcardio, storage
+
+MISO, CS, SCK, MOSI = board.GP8, board.GP9, board.GP10, board.GP11
+
+spi = busio.SPI(board.GP10, MOSI=board.GP11, MISO=board.GP8)
+
+sdcard = sdcardio.SDCard(spi, CS)
+vfs = storage.VfsFat(sdcard)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+
+os.listdir()
